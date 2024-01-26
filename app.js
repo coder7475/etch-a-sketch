e71274871b95d1af62a138ad116286637b6b940b
coder7475 <robiulhossain7475@gmail.com>
2024-01-26 23:34:54 +0600

    feat: create a 16x16 grid of square divs using js and css

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const grids = document.getElementById("grids");
 
-const div = document.createElement("div");
-grids.appendChild(div);
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    grids.appendChild(div);
+  }
+}
