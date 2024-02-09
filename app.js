045d959703f5936d8418f12e86c811d47021ba46
coder7475 <robiulhossain7475@gmail.com>
2024-02-09 19:59:19 +0600

    feat: refactor code: create a function to generate grids based on number

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const grids = document.getElementById("grids");
 const chgBtn = document.getElementById("change-btn");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    grids.appendChild(div);
+const generateGrids = (gridNums) => {
+  for (let i = 0; i < gridNums; i++) {
+    for (let j = 0; j < gridNums; j++) {
+      let div = document.createElement("div");
+      grids.appendChild(div);
+    }
   }
 }
 
+//? MAIN
+// generate default grid
+generateGrids(16);
+
+// add event listener
 chgBtn.addEventListener("click", () => {
   let gridNum = parseInt(prompt("Enter a Value:"));
   while (typeof(gridNum) !== "number" || (gridNum > 100 && gridNum < 1)) {
@@ -15,13 +22,8 @@ chgBtn.addEventListener("click", () => {
   }
 
   removeAllChildNodes(grids);
-
-  for (let i = 0; i < gridNum; i++) {
-    for (let j = 0; j < gridNum; j++) {
-      let div = document.createElement("div");
-      grids.appendChild(div);
-    }
-  }
+  generateGrids(gridNum);
+  
 })
 
 // create function to remove all childs
