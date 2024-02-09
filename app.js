1a95e3ae7caf9010106ca79eb514e05c2765a9f3
coder7475 <robiulhossain7475@gmail.com>
2024-02-09 19:23:02 +0600

    feat: add a event listener to btn to open a modal to ask for a input value for grid size

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const grids = document.getElementById("grids");
+const chgBtn = document.getElementById("change-btn");
+
+chgBtn.addEventListener("click", () => {
+  const gridNum = prompt("Enter a Value:");
+  console.log(gridNum);
+})
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
