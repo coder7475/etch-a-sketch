2832dfe23641ae07f6b9816b5874c39fdb46b932
coder7475 <robiulhossain7475@gmail.com>
2024-02-09 19:54:47 +0600

    feat: create a function to remove all child node in a container node

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const grids = document.getElementById("grids");
 const chgBtn = document.getElementById("change-btn");
 
-chgBtn.addEventListener("click", () => {
-  let gridNum = prompt("Enter a Value:", 16);
-  while (typeof(gridNum) !== "number" || (gridNum > 100 && gridNum < 1)) {
-    gridNum = parseInt(prompt("Enter a Value:", 16));
-  }
-
-  console.log(gridNum);
-})
-
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let div = document.createElement("div");
     grids.appendChild(div);
+  }
+}
+
+chgBtn.addEventListener("click", () => {
+  let gridNum = parseInt(prompt("Enter a Value:"));
+  while (typeof(gridNum) !== "number" || (gridNum > 100 && gridNum < 1)) {
+    gridNum = parseInt(prompt("Enter a Value:"));
+  }
+
+  removeAllChildNodes(grids);
+
+  for (let i = 0; i < gridNum; i++) {
+    for (let j = 0; j < gridNum; j++) {
+      let div = document.createElement("div");
+      grids.appendChild(div);
+    }
+  }
+})
+
+// create function to remove all childs
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
   }
 }
