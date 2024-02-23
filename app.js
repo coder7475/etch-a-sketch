9937382b8f65289caf8d818fd2281e8b297bb41e
coder7475 <robiulhossain7475@gmail.com>
2024-02-23 19:43:41 +0600

    feat: all previeous changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// constants
 const grids = document.getElementById("grids");
 const chgBtn = document.getElementById("change-btn");
 
+
+// functions
 const generateGrids = (gridNums) => {
   for (let i = 0; i < gridNums; i++) {
     for (let j = 0; j < gridNums; j++) {
@@ -10,11 +13,19 @@ const generateGrids = (gridNums) => {
   }
 }
 
+// create function to remove all childs
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
 //? MAIN
 // generate default grid
 generateGrids(16);
 
 // add event listener
+// generate grids dynamically
 chgBtn.addEventListener("click", () => {
   let gridNum = parseInt(prompt("Enter a Value:"));
   while (typeof(gridNum) !== "number" || (gridNum > 100 && gridNum < 1)) {
@@ -26,9 +37,3 @@ chgBtn.addEventListener("click", () => {
   
 })
 
-// create function to remove all childs
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-  }
-}
